a9d21199fa8576a7db19dbe53937242f967dfe9e
Linus Zackrisson <linus.gz@hotmail.com>
2024-05-13 14:35:17 +0200

    redone

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { getGamesData } from './api';
-import StickyNav from './StickyNav';
-import HeroSection from './HeroSection';
-import { Routes, Route } from 'react-router-dom'; // Import Routes
-import GameListPage from './GameListPage';
-import Carousel from './Carousel';
+import React from 'react'
+import HeroSection from './HeroSection'
 
-
-const App = () => {
-  const [games, setGames] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const gamesData = await getGamesData();
-        setGames(gamesData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+function App() {
   return (
     <div>
-      < StickyNav/>
-      
-    <Routes>
-      <Route path="/" element={<HeroSection />} />
-      
-        <Route path="/gamelist" element={<GameListPage />} />
-
-       </Routes>
-      
+    < HeroSection/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,22 +1,55 @@
-import React from 'react';
-import Carousel from './Carousel'; // Importera din Carousel-komponent här
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const HeroSection = () => {
+function HeroSection() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchGames = async () => {
+      const options = {
+        method: 'GET',
+        url: 'https://free-to-play-games-database.p.rapidapi.com/api/filter',
+        params: {
+          tag: '3d.mmorpg.fantasy.pvp',
+          platform: 'pc'
+        },
+        headers: {
+          'X-RapidAPI-Key': 'c76a1e2636msh45aef8a068997cep132b07jsn88f2e2dc26be',
+          'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        setGames(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchGames();
+  }, []);
+
   return (
-    <div className="bg-surface-100 text-white h-screen flex-col flex items-center justify-center relative flex">
-      <div className="absolute inset-0 bg-gradient-to-br from-green-700 to-transparent opacity-25"></div>
-      <div className="max-w-7xl mx-auto px-6 py-16 relative z-10 flex flex-col items-center">
-        <h1 className="text-6xl font-bold mb-4 text-center">
-          <span className="bg-gradient-to-r from-green-600 to-green-200 text-transparent bg-clip-text">Free 2 Play: Where the Only Investment is Your Sleep Schedule</span>
-        </h1>
-        <p className="text-2xl text-center mb-8">Dive into our array of free-to-play games or leave it to the roll of the dice.</p>
-        <div className="flex justify-center">
-          <button className="mr-4 bg-gradient-to-r from-green-800 to-green-500 hover:from-green-700 hover:to-green-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Explore</button>
-          <button className="bg-gradient-to-r from-green-800 to-green-500 hover:from-green-700 hover:to-green-400 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Roll the Dice</button>
+    <div className="Hero-container bg-gradient-to-r from-emerald-500 to-emerald-900">
+      <div className='max-w-7xl flex-col flex items-center justify-center h-screen mx-auto'>
+        <h1 className='text-center text-6xl font-bold'>Free 2 Play: Where the Only Investment is Your Sleep Schedule</h1>
+        <h2 className='mt-4 text-2xl'>Dive into our array of free-to-play games or leave it to the roll of the dice.</h2>
+        <div className="button-container flex-row mt-4">
+          <button className='mr-4 bg-blue-600 px-4 py-2 rounded font-semibold'>Explore</button>
+          <button className='bg-blue-600 px-4 py-2 rounded font-semibold'>Roll the Dice</button>
+        </div>
+        <div className="games-container mt-8">
+          {games.map(game => (
+            <div key={game.id} className="game">
+              <h3>{game.title}</h3>
+              <p>{game.description}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default HeroSection;
